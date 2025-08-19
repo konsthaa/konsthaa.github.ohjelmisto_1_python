fef7dd2a43ef88a2754d29b1d8381e38a964c2c0
konsthaa <konsthaa@metropolia.fi>
2025-08-19 13:57:30 +0300

    Initial commit

---
+++ mod01/01.py
@@ -0,0 +1,16 @@
+# Moikka olen kommentti
+print('Terve, Konsta')
+print('mene töihin')
+print('Hei, Konsta Haapaniemi')
+print("moi 'Konsta'")
+print("Hyvää Huomenta \nKonsta")
+print(8)
+
+# yksinkertainen muuttuja
+nimi= 'Konsta'
+print(nimi)
+
+
+# syöte, eli tietoa käyttäjältä
+nimi2=input('Moikka, anna nimesi: ')
+print(nimi2)
