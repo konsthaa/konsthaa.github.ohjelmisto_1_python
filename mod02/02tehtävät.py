a7867d16a4f42e6a4749ab76e971c4fc3c82b499
konsthaa <konsthaa@metropolia.fi>
2025-08-20 13:49:26 +0300

    mod 2 tunnin muistiinpanot mod2tunti.py:ssa.
    Osa 02.tehtävistä tehty

--- mod02/02tehtävät.py
+++ mod02/02tehtävät.py
@@ -44,11 +44,13 @@ import math
 # Tehtävä 5 Kirjoita ohjelma, joka kysyy käyttäjältä massan keskiaikaisten mittojen mukaan leivisköinä, nauloina ja luoteina.
 #           Ohjelma muuntaa syötteen täysiksi kilogrammoiksi ja grammoiksi sekä ilmoittaa tuloksen käyttäjälle.
 
-
 # Yksi leiviskä on 20 naulaa.
 # Yksi naula on 32 luotia.
 # Yksi luoti on 13,3 grammaa.
 
+luoti = print(13.3)
+naula = print(13.3 * 32)
+lieviskä
 
 
 
