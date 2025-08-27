5e12634692f66f1f2d7a5f5548720ee98146912b
konsthaa <konsthaa@metropolia.fi>
2025-08-27 11:37:54 +0300

    mod 04 tehtävät mod04-tehtävät.py

---
+++ mod04/mod04-tehtävät.py
@@ -0,0 +1,41 @@
+import random
+# tehtävä 1
+# Kirjoita while-toistorakennetta käyttävä ohjelma, joka tulostaa kolmella jaolliset luvut väliltä 1..1000.
+
+
+#i = 1
+#while i <= 1000:
+    #if i % 3 == 0:
+        #print(f"i={i}")
+    #i += 1
+
+# tehtävä 2
+# Kirjoita ohjelma, joka muuntaa tuumia senttimetreiksi niin kauan kunnes käyttäjä antaa negatiivisen tuumamäärän.
+# Sen jälkeen ohjelma lopettaa toimintansa.
+# 1 tuuma = 2,54 cm
+
+i = 1
+x = i = 2.54
+while i >= 0:
+    print(f"{}")
+
+
+
+
+#teht 6
+
+# kaikkien pisteiden määrä N
+#N = 10
+# ympyrän sisään osuvien pisteiden lkm n
+#n = 0
+#i = 0
+#while i < N:
+    #i = i + 1
+    # arvotaan satunnainen piste koordinaatistoon
+    #x = random.uniform(-1, 1)
+    #y = random.uniform(-1, 1)
+    #print(f"Arvottu piste: {x}, {y}")
+    # TODO: testaa, toteutuuko piste epäyhtälön x^2+y^2 < 1
+    # jos ehtgo on totta, kasvata n-laskurin arvoa
+
+#TODO: laske ja tulosta piin likiarvo käyttäen kaavaa: pi\approx4n/N etsi oikea kaava.
