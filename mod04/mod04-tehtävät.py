6cb372660b0eabd9d0cf56a549be1f5f4bc95ae1
konsthaa <konsthaa@metropolia.fi>
2025-08-31 00:45:21 +0300

    mod 04 tehtävät mod04-tehtävät.py

--- mod04/mod04-tehtävät.py
+++ mod04/mod04-tehtävät.py
@@ -1,27 +1,27 @@
-# tehtävä 1
-# Kirjoita while-toistorakennetta käyttävä ohjelma, joka tulostaa kolmella jaolliset luvut väliltä 1..1000.
-#from mod03.noppapeli import app_running
+#tehtävä 1
+#Kirjoita while-toistorakennetta käyttävä ohjelma, joka tulostaa kolmella jaolliset luvut väliltä 1..1000.
+
 import random
 
+import math
 
-
-#i = 1
-#while i <= 1000:
-    #if i % 3 == 0:
-        #print(f"i={i}")
-    #i += 1
+i = 1
+while i <= 1000:
+    if i % 3 == 0:
+        print(f"i={i}")
+    i += 1
 
 # tehtävä 2
 # Kirjoita ohjelma, joka muuntaa tuumia senttimetreiksi niin kauan kunnes käyttäjä antaa negatiivisen tuumamäärän.
 # Sen jälkeen ohjelma lopettaa toimintansa.
 # 1 tuuma = 2,54 cm
 
-#tuumat = float(input("Anna tuumien määrä (negatiivinen luku lopettaa): "))
-#while tuumat >= 0:
-    #sentit = tuumat * 2.54
-    #print(f"{tuumat} tuumaa = {sentit:.2f} cm")
-    #tuumat = float(input("Anna tuumat (negatiivinen luku lopettaa): "))
-#print("Ohjelma lopetettu.")
+tuumat = float(input("Anna tuumien määrä (negatiivinen luku lopettaa): "))
+while tuumat >= 0:
+    sentit = tuumat * 2.54
+    print(f"{tuumat} tuumaa = {sentit:.2f} cm")
+    tuumat = float(input("Anna tuumat (negatiivinen luku lopettaa): "))
+print("Ohjelma lopetettu.")
 
 
 
@@ -29,19 +29,20 @@ import random
 # Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka, kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
 # Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
 
-#numerot = []
-#app_running = True
-#while app_running:
-    #luku = input("Anna luku (enter lopettaa): ")
-    #if luku == "":
-        #print("Error, tyhjä merkkijono")
-        #app_running = False
-    #numerot.append(luku)
-#if numerot:
-    #print(f"pienin luku {min(numerot)}:")
-    #print(f"suurin luku {max(numerot)}:")
-#else:
-    #print("Loppu")
+numerot = []
+app_running = True
+while app_running:
+    luku = input("Anna luku (enter lopettaa): ")
+    if luku == "":
+        print("Error, tyhjä merkkijono")
+        app_running = False
+    else:
+        numerot.append(luku)
+if numerot:
+    print(f"pienin luku {min(numerot)}:")
+    print(f"suurin luku {max(numerot)}:")
+else:
+    print("Loppu")
 
 
 # tehtävä 4
@@ -50,32 +51,72 @@ import random
 # Kunkin arvauksen jälkeen ohjelma tulostaa tekstin Liian suuri arvaus, Liian pieni arvaus tai Oikein.
 # Huomaa, että tietokone ei saa vaihtaa lukuaan arvauskertojen välissä.
 
-#numerot = random.randint(1,10) #arvotaan satunnainen numero (1-10).
+numerot = random.randint(1,10) #arvotaan satunnainen numero (1-10).
 
-#while True:
-    #luku = int(input("Anna luku (1-10): "))   # pelaaja arvaa (1-10)
+while True:
+    luku = int(input("Anna luku (1-10): "))   # pelaaja arvaa (1-10)
 
-    #if luku > numerot:
-        #print("Liian suuri arvaus!")
-    #elif luku < numerot:
+    if luku > numerot:
+        print("Liian suuri arvaus!")
+    elif luku < numerot:
         print("Liian pieni arvaus!")
     else:
         print("Oikein!")
         break
+
+# tehtävä 5
+# Kirjoita ohjelma, joka kysyy käyttäjältä käyttäjätunnuksen ja salasanan.
+# Jos jompikumpi tai molemmat ovat väärin, tunnus ja salasana kysytään uudelleen.
+# Tätä jatketaan kunnes kirjautumistiedot ovat oikein tai väärät tiedot on syötetty viisi kertaa.
+# Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty. (Oikea käyttäjätunnus on python ja salasana rules).
+
+oikea käyttäjätunnus = konstah
+oikea salasana = salakala
+
+maksimiyritykset = 5
+yritykset = 0
+while yritykset < maksimiyritykset:
+    tunnus = input("Anna käyttäjätunnus: ")
+    salasana = input("Anna salasana: ")
+
+    yritykset += 1
+
+    if tunnus == "python" and salasana == "rules":
+        print("Tervetuloa")
+        break
+    if tunnus != "python" and salasana == "rules":
+        print("Pääsy evätty")
+
+    if tunnus == "python" and salasana != "rules":
+        print("Pääsy evätty")
+
+    if tunnus != "python" and salasana != "rules":
+        print("Pääsy evätty")
+# jos yrityksiä tulee yli viisi ilman onnistunutta kirjautumista:'
+if yritykset == maksimiyritykset:
+    print("")
+
 #teht 6
 
 # kaikkien pisteiden määrä N
-#N = 10
+N = 10
 # ympyrän sisään osuvien pisteiden lkm n
-#n = 0
-#i = 0
-#while i < N:
-    #i = i + 1
-    # arvotaan satunnainen piste koordinaatistoon
-    #x = random.uniform(-1, 1)
-    #y = random.uniform(-1, 1)
-    #print(f"Arvottu piste: {x}, {y}")
-    # TODO: testaa, toteutuuko piste epäyhtälön x^2+y^2 < 1
-    # jos ehtgo on totta, kasvata n-laskurin arvoa
+n = 0
+i = 0
+while i < N:
+    i = i + 1
 
-#TODO: laske ja tulosta piin likiarvo käyttäen kaavaa: pi\approx4n/N etsi oikea kaava.
+    # arvotaan satunnainen piste koordinaatistoon
+
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    print(f"Arvottu piste: {x}, {y}")
+
+    if x**2 + y**2 < 1 == True:   # jos ehto on totta, kasvatetaan n-laskurin arvoa
+        n = n + 1
+
+#lasketaan pi:n likiarvo:
+    Pi_likiarvo = 4 * (n / N)
+    print(f"piin likiarvo {Pi_likiarvo}")
+
+
