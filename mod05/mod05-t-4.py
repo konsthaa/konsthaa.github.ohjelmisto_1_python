f842b6c06eaebc3163461be326e689aee0ed61a2
konsthaa <konsthaa@metropolia.fi>
2025-09-06 01:02:11 +0300

    mod 05 tehtävät

---
+++ mod05/mod05-t-4.py
@@ -0,0 +1,14 @@
+# tehtävä 4
+# Kirjoita ohjelma, joka kysyy käyttäjältä viiden kaupungin nimet yksi kerrallaan
+# (käytä for-toistorakennetta nimien kysymiseen) ja tallentaa ne listarakenteeseen.
+# Lopuksi ohjelma tulostaa kaupunkien nimet yksi kerrallaan allekkain samassa järjestyksessä kuin ne syötettiin.
+# Käytä for-toistorakennetta nimien kysymiseen ja for/in toistorakennetta niiden läpikäymiseen.
+
+kaupungit = []
+
+for i in range(5):
+    kaupunki = input(f"Syötä {i+1}. kaupungin nimi: ")
+    kaupungit.append(kaupunki)
+
+for kaupunki in kaupungit:
+    print(kaupunki)
